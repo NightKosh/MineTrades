4cfa306a25c95fd4b902499300480e2afafd89b7
NightKosh <nightkosh@gmail.com>
2019-08-07 04:13:27 +0300

    backgrounds

--- js/global.js
+++ js/global.js
@@ -1,5 +1,5 @@
 
-app.controller("GlobalPageCtrl", ['$scope', '$http', 'PageService', function($scope, $http, PageService) {
+app.controller("GlobalPageCtrl", ['$scope', '$http', '$interval', 'PageService', function($scope, $http, $interval, PageService) {
     $scope.globalData = {};
 
     $http.get('data/global/' + PageService.getPageParams().lang + '.json?v=' + version).success(function (data, status, headers, config) {
@@ -161,4 +161,70 @@ app.controller("GlobalPageCtrl", ['$scope', '$http', 'PageService', function($sc
             price: 133
         }
     ];
+
+    var bgImages = [
+        "emerald",
+        "diamond",
+        "gold",
+        "iron-ore",
+        "acacia-wood",
+        "andesite",
+        "bookshelf",
+        "bricks",
+        "cactus",
+        "chest",
+        "coal-ore",
+        "crafting-table",
+        "diamond-ore",
+        "dispenser",
+        "diorite",
+        "furnace",
+        "glowstone",
+        "gold-ore",
+        "grass",
+        "hay",
+        "lava",
+        "lazuli",
+        "leaves",
+        "magma",
+        "melon",
+        "moss-stone",
+        "mushroom",
+        "mycelium",
+        "nether-brick",
+        "netherrack",
+        "note-block",
+        "observer",
+        "obsidian",
+        "piston",
+        "podzol",
+        "prismarine",
+        "pumpkin",
+        "purpur",
+        "quartz",
+        "granite",
+        "redstone",
+        "redstone-lamp",
+        "sand",
+        "sandstone",
+        "sea-lantern",
+        "slime",
+        "soul-sand",
+        "sponge",
+        "tnt",
+        "water",
+        "stone",
+        "spruce-wood",
+        "bedrock",
+        "birch-wood",
+        "bone",
+        "end-stone"
+    ];
+    $scope.bgPos = 0;
+    $scope.class = bgImages[$scope.bgPos];
+    // $scope.class = bgImages[9];
+    $interval(function() {
+        $scope.bgPos = ($scope.bgPos == bgImages.length - 1) ? 0 : $scope.bgPos + 1;
+        $scope.class = bgImages[$scope.bgPos];
+    }, 5000);
 }]);
